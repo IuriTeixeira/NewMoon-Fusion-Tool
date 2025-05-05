f5ac2a253911649d9bd81565813311c163979376
Iuri Teixeira <irt@icomp.ufam.edu.br>
2025-05-05 10:13:45 -0400

    Finished law demons on demons.json

--- src/app/Components/DemonTableComponent.tsx
+++ src/app/Components/DemonTableComponent.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Flex, List, Table } from '@mantine/core'
+import { Flex, Text, Table } from '@mantine/core'
 import demonList from '../Data/demons.json'
 import React from 'react'
 
@@ -9,44 +9,50 @@ export default function DemonTableComponent() {
             <Table.Thead>
                 <Table.Tr>
                     <Table.Th>
-                        <Flex align='center'>Race</Flex>
+                        <Flex align='center' justify='center'>Race</Flex>
                     </Table.Th>
                     <Table.Th>
-                        <Flex align='center'>Name</Flex>
+                        <Flex align='center' justify='center'>Name</Flex>
                     </Table.Th>
                     <Table.Th>
-                        <Flex align='center'>Level</Flex>
+                        <Flex align='center' justify='center'>Level</Flex>
                     </Table.Th>
                     <Table.Th>
-                        <Flex align='center'>Fusion Range</Flex>
+                        <Flex align='center' justify='center'>Fusion Range</Flex>
                     </Table.Th>
                     <Table.Th>
-                        <Flex align='center'>Plugin</Flex>
+                        <Flex align='center' justify='center'>Plugin</Flex>
                     </Table.Th>
                 </Table.Tr>
             </Table.Thead>
             <Table.Tbody>
                 {demonList.map((demon, index) => {
                     return (
-                        <Table.Tr>
+                        <Table.Tr key={`row-${index}`}>
                             <Table.Td key={`race-${index}`}>{demon.Race}</Table.Td>
                             <Table.Td key={`name-${index}`}>{demon.Name}</Table.Td>
-                            <Table.Td key={`level-${index}`}>{demon.Level}</Table.Td>
-                            {demon.Range &&
+                            <Table.Td key={`level-${index}`}><Flex key={`level-flex-${index}`} align='center' justify='center'>{demon.Level}</Flex></Table.Td>
+                            {demon.Range && typeof(demon.Range[0]) === 'number' &&
                                 <Table.Td key={`range-${index}`}>
-                                    {demon.Range[0]}{demon.Range[1] && ` - ${demon.Range[1]}`}{!demon.Range[1] && '+'} 
+                                    {demon.Range[0]}{demon.Range[1] && ` - ${demon.Range[1]}`}{!demon.Range[1] && '+'}
                                 </Table.Td>}
-                            {!demon.Range &&
+                            {demon.Range && typeof(demon.Range[0]) !== 'number' &&
+                                <Table.Td key={`range-${index}`}>
+                                    {<Text key={`no-fusion-range-text-${index}`} c='red' size='sm'>{demon.Range[0]}</Text>}
+                                </Table.Td>}
+                            {!demon.Range && demon.Special &&
                                 <Table.Td key={`special-${index}`}>
-                                    {demon.Special.map((line, index2) => (
-                                        <React.Fragment key={`fragment-${index}-${index2}`}>
-                                            {line[0]} x {line[1]} {line[2] && `x ${line[2]}`}
-                                            <br key={`br-${index}-${index2}`} />
+                                    {demon.Special.map((line, indexSpecial) => (
+                                        <React.Fragment key={`fragment-${index}-${indexSpecial}`}>
+                                            {line[0]}
+                                            {line[1] && ` x ${line[1]}`}
+                                            {line[2] && ` x ${line[2]}`}
+                                            <br key={`br-${index}-${indexSpecial}`} />
                                         </React.Fragment>
                                     ))}
                                 </Table.Td>}
-                            {demon.Plugin && <Table.Td key={`plugin-${index}`}>Yes</Table.Td>}
-                            {!demon.Plugin && <Table.Td key={`plugin-${index}`}>No</Table.Td>}
+                            {demon.Plugin && <Table.Td key={`plugin-${index}`}><Flex key={`plugin-flex-${index}`} align='center' justify='center'>Yes</Flex></Table.Td>}
+                            {!demon.Plugin && <Table.Td key={`plugin-${index}`}><Flex key={`plugin-flex-${index}`} align='center' justify='center'>No</Flex></Table.Td>}
                         </Table.Tr>
                     )
                 })}
