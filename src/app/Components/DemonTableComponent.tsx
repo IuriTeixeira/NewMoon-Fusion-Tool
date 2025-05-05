2ca0ce7da28c53102980e094def30d3611002394
Iuri Teixeira <irt@icomp.ufam.edu.br>
2025-05-05 11:37:17 -0400

    Added some neutral demons to demons.json

--- src/app/Components/DemonTableComponent.tsx
+++ src/app/Components/DemonTableComponent.tsx
@@ -32,13 +32,13 @@ export default function DemonTableComponent() {
                             <Table.Td key={`race-${index}`}>{demon.Race}</Table.Td>
                             <Table.Td key={`name-${index}`}>{demon.Name}</Table.Td>
                             <Table.Td key={`level-${index}`}><Flex key={`level-flex-${index}`} align='center' justify='center'>{demon.Level}</Flex></Table.Td>
-                            {demon.Range && typeof(demon.Range[0]) === 'number' &&
+                            {demon.Range && typeof (demon.Range[0]) === 'number' &&
                                 <Table.Td key={`range-${index}`}>
                                     {demon.Range[0]}{demon.Range[1] && ` - ${demon.Range[1]}`}{!demon.Range[1] && '+'}
                                 </Table.Td>}
-                            {demon.Range && typeof(demon.Range[0]) !== 'number' &&
+                            {demon.Range && typeof (demon.Range[0]) !== 'number' &&
                                 <Table.Td key={`range-${index}`}>
-                                    {<Text key={`no-fusion-range-text-${index}`} c='red' size='sm'>{demon.Range[0]}</Text>}
+                                    <Text key={`no-fusion-range-text-${index}`} c='red' size='sm'>{demon.Range[0]}</Text>
                                 </Table.Td>}
                             {!demon.Range && demon.Special &&
                                 <Table.Td key={`special-${index}`}>
@@ -51,8 +51,11 @@ export default function DemonTableComponent() {
                                         </React.Fragment>
                                     ))}
                                 </Table.Td>}
-                            {demon.Plugin && <Table.Td key={`plugin-${index}`}><Flex key={`plugin-flex-${index}`} align='center' justify='center'>Yes</Flex></Table.Td>}
-                            {!demon.Plugin && <Table.Td key={`plugin-${index}`}><Flex key={`plugin-flex-${index}`} align='center' justify='center'>No</Flex></Table.Td>}
+                            <Table.Td key={`plugin-${index}`}>
+                                <Flex key={`plugin-flex-${index}`} align='center' justify='center'>
+                                    {demon.Plugin ? "✔️" : "❌"}
+                                </Flex>
+                            </Table.Td>
                         </Table.Tr>
                     )
                 })}
