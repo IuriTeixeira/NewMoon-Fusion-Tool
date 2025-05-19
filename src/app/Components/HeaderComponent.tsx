2b882a8e6c454ac8500d2787ee02df4bc080cdfd
Iuri Teixeira <irt@icomp.ufam.edu.br>
2025-05-19 09:51:04 -0400

    Update HeaderComponent.tsx

--- src/app/Components/HeaderComponent.tsx
+++ src/app/Components/HeaderComponent.tsx
@@ -8,7 +8,7 @@ export default function HeaderComponent() {
             <Container size="md" className={classes.inner}>
                 <Link href={'/'}>
                     <Flex align={'flex-end'} justify={'flex-start'} gap={'xs'}>
-                        <Image src={`/NewMoonLogo.png`} alt='New Moon Logo' w={48} h={48} /> <h1>New Moon Fusion Calculator</h1>
+                        <Image src={`/NewMoonLogo.png`} alt='New Moon Logo' w={48} h={48} /> <h1>New Moon Fusion Tool</h1>
                     </Flex>
                 </Link>
                 <Text c={'dimmed'} fw={500}>by Glorienn</Text>
